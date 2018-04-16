7bd0b0e7d2cbc52ccf033906064b57c1acd010af
Jeff Brubaker <jtbrubak1@gmail.com>
2018-04-16 17:56:20 -0400

    Add unregistered shifts table

---
+++ db/migrate/20180416205044_create_unregistered_shifts.rb
@@ -0,0 +1,13 @@
+class CreateUnregisteredShifts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :unregistered_shifts do |t|
+      t.date :date, null: false
+      t.time :start_time, null: false
+      t.time :end_time, null: false
+      t.integer :store_id, null: false
+      t.string :name, null: false
+      t.string :email, null: false
+      t.timestamps
+    end
+  end
+end
